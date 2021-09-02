a4e688ea1818367ce82042f7ce910808154cccc2
easy4docker <easy4docker@gmail.com>
2021-09-02 05:07:28 -0700

    update

--- app/routes/api.js
+++ app/routes/api.js
@@ -4,15 +4,15 @@ const RESTS = 'get|put|post|delete'.split('|');
 for (let i=0 ; i < RESTS.length; i++) {
   router[RESTS[i]]('/:module/', function(req, res, next) {
     const config = req.app.get('config');
-    delete require.cache[config.root +'/apiModules/index.js'];
-    const appEngine = require(config.root +'/apiModules/index.js');
+    delete require.cache[config.root +'/appModules/api/index.js'];
+    const appEngine = require(config.root +'/appModules/api/index.js');
     appEngine(req, res, next);
     return true;
   });
   router[RESTS[i]]('/:module/:id', function(req, res, next) {
     const config = req.app.get('config');
-    delete require.cache[config.root +'/apiModules/index.js'];
-    const appEngine = require(config.root +'/apiModules/index.js');
+    delete require.cache[config.root +'/appModules/api/index.js'];
+    const appEngine = require(config.root +'/appModules/api/index.js');
     appEngine(req, res, next);
     return true;
   });
--- app/routes/pdf.js
+++ app/routes/pdf.js
@@ -4,8 +4,8 @@ const RESTS = 'get|put|post|delete'.split('|');
 for (let i=0 ; i < RESTS.length; i++) {
   router[RESTS[i]]('/:id', function(req, res, next) {
     const config = req.app.get('config');
-    delete require.cache[config.root +'/pdfModules/customerPDF.js'];
-    const CustomerPDF = require(config.root +'/pdfModules/customerPDF.js');
+    delete require.cache[config.root +'/appModules/service/customerPDF.js'];
+    const CustomerPDF = require(config.root +'/appModules/service/customerPDF.js');
     const custmerPDF = new CustomerPDF(req, res, next);
     custmerPDF.run()
     return true;
