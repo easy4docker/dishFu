0304a16ce5fb4f044656b9a9241571f0f7d2a18a
easy4docker <easy4docker@gmail.com>
2021-09-02 15:26:32 -0700

    update

--- cronJobs/auto-approve/backendApp.js
+++ cronJobs/auto-approve/backendApp.js
@@ -1,4 +1,5 @@
 const tools = require('./tools');
+const md5 = require('md5');
 module.exports = class App extends tools {
     constructor() {
         super();
@@ -15,16 +16,41 @@ module.exports = class App extends tools {
         const me = this;
         const connection = me.mysql.createConnection(me.dbCfg);
         connection.connect();
-    
         const sql = "SELECT * FROM `application`";
         connection.query(sql, function (err, result) {
           if (err) {
             console.log({status: 'failure', message:err.message});
           } else {
-            console.log({status: 'success', data: result});
+              me.process(result);
           }
         });
         connection.end();
-        console.log(me.dbCfg)
+        
+    }
+    process(v) {
+        const values =[];
+        const fields = ['authCode', 'address', 'description', 'roles', 'specialFoodie', 'created', 'status'];
+        for (let i = 0; i < v.length; i++ ) {
+            values.push([
+                md5(v[i].address), 
+                v[i].address, v[i].publisher, 
+                v[i].type, 
+                (v[i].type === 'foodie') ? 1 : 0,
+                new Date(),
+                0
+            ]);
+        }
+        const me = this;
+        const connection = me.mysql.createConnection(me.dbCfg);
+        connection.connect();
+        const sql = "INSERT INTO authUsers (`" + fields.join('`,`') + "`) VALUES ?";
+        connection.query(sql, [values], function (err, result) {
+            if (err) {
+                console.log({status: 'failure', message:err.message});
+            } else {
+              console.log({status: 'success', data: result});
+            }
+          });
+        connection.end();
     }
 }
