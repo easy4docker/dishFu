a9959798a60ff4fe2cfcda0e62d2da4114a467e3
easy4docker <easy4docker@gmail.com>
2021-09-04 15:57:16 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,23 @@
 BASEDIR=$(dirname "$0")
 ROOTDIR="$(PWD)"
 FULLDIR="$(PWD)/${BASEDIR}"
-#/${BASEDIR}"
 
 echo $ROOTDIR
 echo $FULLDIR
+
+docker stop dishfu-container && docker rm dishfu-container  && docker image rm dishfu-image && docker image prune
+docker image build --file ${FULLDIR}/Dockerfile -t dishfu-image .
+
+MAIN_NET="33.33.33"
+MAIN_IP="33.33.33.254"
+docker network create \
+    --driver=bridge \
+    --subnet=${MAIN_NET}.0/16 \
+    --ip-range=${MAIN_NET}.0/24 \
+    --gateway=${MAIN_IP} \
+    network_dishfu &> /dev/null
+
+docker run -v "${FULLDIR}/cronJobs":/var/cronJobs -v "${FULLDIR}/app":/var/app -v "${FULLDIR}/log":/var/log \
+    -v ${ROOTDIR}/_services/dishfuPDF/_cronPDFData:/var/dishfuPDF  -p 3001:3000  \
+    --network network_dishfu --restart on-failure \
+    --name dishfu-container -d dishfu-image
