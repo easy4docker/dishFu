bfc69c3766185c74be3d2da4c9f4d4e598b12a0e
easy4docker <easy4docker@gmail.com>
2021-09-04 22:46:20 -0700

    Update cronOnboot.sh

--- cronJobs/cronOnboot.sh
+++ cronJobs/cronOnboot.sh
@@ -1,3 +1,7 @@
 echo $(date -u) "Ran cronOnboot" 
+mkdir -p /var/dishfuPDF/input/
+mkdir -p /var/dishfuPDF/output/
+mkdir -p /var/dishfuPDF/done/
+mkdir -p /var/dishfuPDF/failed/
 cd /var/app && npm install
 npm start &
