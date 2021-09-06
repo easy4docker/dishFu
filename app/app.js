048677d5b9ce1b63cce4b59305a30bc9408310e0
easy4docker <easy4docker@gmail.com>
2021-09-06 15:28:11 -0700

    Update app.js

--- app/app.js
+++ app/app.js
@@ -29,10 +29,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', indexRouter);
-app.use('/', express.static('public/build'))
+
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 app.use('/pdf', pdfRouter);
+app.use('/', express.static('public/build'))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
