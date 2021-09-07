00d17c046182c30bbcfc108a69f5abea33cd8a80
easy4docker <easy4docker@gmail.com>
2021-09-06 17:29:24 -0700

    Update _setupAPP.sh

--- _setupAPP.sh
+++ _setupAPP.sh
@@ -6,9 +6,9 @@ cd ${FULLDIR}
 mkdir -p ${FULLDIR}/app/config
 cp ${ROOTDIR}/config/app/mysql.json ${FULLDIR}/app/config/mysql.json
 
-docker stop dishfu-container && docker rm dishfu-container  && docker image rm dishfu-image
+docker stop dishfu-app-container && docker rm dishfu-app-container  && docker image rm dishfu-app-image
 # ---  && docker image prune -f
-docker image build --file ${FULLDIR}/Dockerfile -t dishfu-image .
+docker image build --file ${FULLDIR}/Dockerfile -t dishfu-app-image .
 
 MAIN_NET="33.33.33"
 MAIN_IP="33.33.33.254"
@@ -27,4 +27,4 @@ mkdir -p ${ROOTDIR}/_shared/PDF/failed
 docker run -v "${FULLDIR}/cronJobs":/var/cronJobs -v "${FULLDIR}/app":/var/app -v "${FULLDIR}/log":/var/log \
     -v "${ROOTDIR}/_shared/PDF":/var/_shared/PDF  -p 3001:3000  \
     --network network_dishfu --restart on-failure \
-    --name dishfu-container -d dishfu-image
+    --name dishfu-app-container -d dishfu-app-image
