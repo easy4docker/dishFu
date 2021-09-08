9771476fab4aa13baf99751a8b6b73273a61f4a9
easy4docker <easy4docker@gmail.com>
2021-09-07 22:20:05 -0700

    update

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send(req.params);
-  // res.render('index', { title: 'Three Stamps Data Site' });
+  res.render('index', { title: 'Three Stamps Data Site' });
 });
 
 module.exports = router;
