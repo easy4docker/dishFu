73e9374b9c3d452af368fb98b8e0eaea66d71e57
easy4docker <easy4docker@gmail.com>
2021-09-12 07:20:09 -0700

    Update admin.js

--- app/appModules/api/admin.js
+++ app/appModules/api/admin.js
@@ -8,6 +8,15 @@ class Admin {
     delete require.cache[config.root +'/config/mysql.json'];
     this.cfg = require(config.root +'/config/mysql.json').devDB;;
   }
+  makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() *  charactersLength));
+   }
+   return result;
+}
   checkPhone() {
     const me = this;
     const connection = me.mysql.createConnection(me.cfg);
@@ -41,7 +50,7 @@ class Admin {
       const connection = me.mysql.createConnection(me.cfg);
       connection.connect();
       const values = [
-        me.req.body.data.phone, me.req.body.data.visitorId, me.req.body.data.token, new Date().getTime(), new Date()
+        me.req.body.data.phone, me.req.body.data.visitorId, me.req.body.data.token, me.makeid(8), new Date()
       ]
       const sql = "INSERT INTO adminSession (`phone`, `visitorId`, `token`, `authcode`, `created`) VALUES ?";
       connection.query(sql, [[values]], function (err, result) {
