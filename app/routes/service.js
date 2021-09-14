a87e9d51914da21e33acb872d4df69f4ea0d4454
easy4docker <easy4docker@gmail.com>
2021-09-13 23:28:23 -0700

    Update service.js

--- app/routes/service.js
+++ app/routes/service.js
@@ -12,7 +12,9 @@ for (let i=0 ; i < RESTS.length; i++) {
   });
   router[RESTS[i]]('/:action/:code/:other/:more', (req, res, next)=> {
     if (['PDF', 'APP'].indexOf(req.params.action) !== -1) {
-      res.send(req.params)
+      res.writeHead(301, {"Location": "http://192.168.86.126:3006/"});
+      res.end();
+      // res.send(req.params)
     } else {
       res.send({status: 'failure',  message: 'Action Error!'});
     }
