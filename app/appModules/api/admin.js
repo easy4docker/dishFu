b275673909d075e7e71401b9020193089e2c6ba6
easy4docker <easy4docker@gmail.com>
2021-09-15 22:01:42 -0700

    Update admin.js

--- app/appModules/api/admin.js
+++ app/appModules/api/admin.js
@@ -54,7 +54,20 @@ class Admin {
         .create({ 
            body: 'clike the link =2=> one more test http://192.168.86.126:3006/',  
            messagingServiceSid: messagingServiceSid,      
-           to: '+1' + me.req.body.data.phone 
+           to: '+' + me.req.body.data.phone 
+         }) 
+        .then(message => message)
+        .done((message)=> {
+          if (message) {
+            me.res.send({status: 'success', data: message});
+          }
+        });
+      /*  
+        client.messages 
+        .create({ 
+           body: 'clike the link =2=> one more test http://192.168.86.126:3006/',  
+           messagingServiceSid: messagingServiceSid,      
+           to: '+' + me.req.body.data.phone 
          }) 
         .then(message => message)
         .catch(err => {
@@ -64,7 +77,7 @@ class Admin {
             me.res.send({status: 'success', data: message});
           }
         });
-        
+        */
     }
   }
 
