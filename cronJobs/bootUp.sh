a6e6abce01b74bc74ac1223b2ceccb80fd73a696
easy4docker <76085700+easy4docker@users.noreply.github.com>
2021-09-15 19:53:13 -0700

    Update bootUp.sh

--- cronJobs/bootUp.sh
+++ cronJobs/bootUp.sh
@@ -4,6 +4,7 @@ mkdir -p /var/dishfuPDF/output/
 mkdir -p /var/dishfuPDF/done/
 mkdir -p /var/dishfuPDF/failed/
 cd /var/app && npm install
+npm audit fix --force
 # npm start &
 cron start &
 echo "==== boot up ==== $(date -u ==="
