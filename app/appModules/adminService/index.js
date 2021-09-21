8c6be1e803fe31f3c0733d5d907997da383d6d16
easy4docker <easy4docker@gmail.com>
2021-09-20 22:37:44 -0700

    update

--- app/appModules/adminService/index.js
+++ app/appModules/adminService/index.js
@@ -6,13 +6,46 @@ class AdminService {
     this.mysql = require('mysql');
     const config = this.req.app.get('config');
     delete require.cache[config.root +'/config/mysql.json'];
-    this.cfg = require(config.root +'/config/mysql.json').devDB;;
+    this.cfg = require(config.root +'/config/mysql.json').devDB;
+    this.QRCode = require('qrcode');
+    this.fs = require('fs');
+  }
+  tpl = (str, vars) =>{
+    var func = new Function(...Object.keys(vars),  "return `"+ str +"`;")
+    return func(...Object.values(vars));
+  }
+  sendPDF(rec) {
+    const me = this;
+    const fnDoc = __dirname + '/tpl/mailQrCodeDoc.html';
+    const linkUrl = 'http://192.168.86.126:3006/adminAuth/';
+    me.QRCode.toDataURL(linkUrl, { 
+      width:256,
+      type: 'image/png',
+      quality: 1.0,
+      color: {
+          dark: '#000000',  
+          light: '#0000'
+      }
+    }, (err, str)=>{
+
+        me.fs.readFile(fnDoc, 'utf-8', (err, doc)=> {
+          try {
+            const html = me.tpl(doc, {linkUrl: linkUrl, qrCode : str});
+            const htmlFn = '/var/_shared/PDF/input/authUserMail.html';
+            me.fs.writeFile(htmlFn, html, (err, doc)=> {
+              me.res.send(html);
+            });
+          } catch (err) {
+            me.res.send(err.message + '=>' + doc);
+          }
+        });
+    });
   }
   call() {
     const me = this;
     switch(me.req.params.action) {
       case 'authedUser' :
-        me.res.send('me.makeid(32)--');
+        me.sendPDF();
         break;
      
       default:  
