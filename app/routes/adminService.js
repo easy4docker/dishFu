34267b50e8195c86700014fe192347a7d44c9edd
easy4docker <easy4docker@gmail.com>
2021-09-20 22:18:06 -0700

    Update adminService.js

--- app/routes/adminService.js
+++ app/routes/adminService.js
@@ -5,31 +5,31 @@ for (let i=0 ; i < RESTS.length; i++) {
   router[RESTS[i]]('/:action/', (req, res, next)=> {
     const config = req.app.get('config');
     delete require.cache[config.root +'/appModules/adminService/index.js'];
-    const ServiceEngine = require(config.root +'/appModules/dminService/index.js');
+    const ServiceEngine = require(config.root +'/appModules/adminService/index.js');
     const serviceEngine = new ServiceEngine(req, res, next);
     serviceEngine.call();
     return true;
   });
   router[RESTS[i]]('/:action/:code', (req, res, next)=> {
     const config = req.app.get('config');
-    delete require.cache[config.root +'/appModules/dminService/index.js'];
-    const ServiceEngine = require(config.root +'/appModules/dminService/index.js');
+    delete require.cache[config.root +'/appModules/adminService/index.js'];
+    const ServiceEngine = require(config.root +'/appModules/adminService/index.js');
     const serviceEngine = new ServiceEngine(req, res, next);
     serviceEngine.call();
     return true;
   });
   router[RESTS[i]]('/:action/:code/:other', (req, res, next)=> {
     const config = req.app.get('config');
-    delete require.cache[config.root +'/appModules/dminService/index.js'];
-    const ServiceEngine = require(config.root +'/appModules/dminService/index.js');
+    delete require.cache[config.root +'/appModules/adminService/index.js'];
+    const ServiceEngine = require(config.root +'/appModules/adminService/index.js');
     const serviceEngine = new ServiceEngine(req, res, next);
     serviceEngine.call();
     return true;
   });
   router[RESTS[i]]('/:action/:code/:other/:more', (req, res, next)=> {
     const config = req.app.get('config');
-    delete require.cache[config.root +'/appModules/dminService/index.js'];
-    const ServiceEngine = require(config.root +'/appModules/dminService/index.js');
+    delete require.cache[config.root +'/appModules/adminService/index.js'];
+    const ServiceEngine = require(config.root +'/appModules/adminService/index.js');
     const serviceEngine = new ServiceEngine(req, res, next);
     serviceEngine.call();
     return true;
