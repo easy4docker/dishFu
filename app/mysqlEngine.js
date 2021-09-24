7cf3d5242a28da809566bf977b920e0b49b6a32d
easy4docker <easy4docker@gmail.com>
2021-09-24 16:39:51 -0700

    Update mysqlEngine.js

--- app/mysqlEngine.js
+++ app/mysqlEngine.js
@@ -21,6 +21,7 @@ module.exports = class mysqlEngine {
         
     }
     queryInsert(sql, data, callback) {
+        
         const me = this;
         me.connection.connect();
         me.connection.query(sql, data, (err, result)=> {
