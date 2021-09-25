aeb28362f760df1a21f77eef129bbae14d622400
easy4docker <easy4docker@gmail.com>
2021-09-24 23:13:45 -0700

    Update usersAdmin.js

--- app/appModules/api/adminApp/usersAdmin.js
+++ app/appModules/api/adminApp/usersAdmin.js
@@ -3,44 +3,24 @@ class usersAdmin {
       this.req = req;
       this.res = res;
       this.next = next;
-      this.mysql = require('mysql');
-      const config = this.req.app.get('config');
-      this.rootpath = config.root;
-      delete require.cache[config.root +'/config/mysql.json'];
-      this.cfg = require(config.root +'/config/mysql.json').devDB;
     }
     getAuthUsers() {
       const me = this;
-      const connection = me.mysql.createConnection(me.cfg);
-      connection.connect();
-      const sql = "SELECT * FROM `authUsers` WHERE 1"
-      connection.query(sql, function (err, result, fields) {
-        if (err) {
-          me.res.send({status: 'failure', message:err.message});
-        } else {
-          if (result && result.length) {
-            me.res.send({status: 'success', data: result});
-          }
-        }
+      const eng = me.req.app.get('mysqlEngine');
+      const sql = "SELECT * FROM `authUsers` WHERE 1";
+      eng.queryOnly(sql, (resultData)=> {
+        me.res.send(resultData);
       });
-      connection.end();
     }
     getAuthUserById() {
       const me = this;
-      const connection = me.mysql.createConnection(me.cfg);
-      connection.connect();
+      const eng = me.req.app.get('mysqlEngine');
       const sql = "SELECT * FROM `authUsers` WHERE `id` = '" + 
         (!me.req.body || !me.req.body.data ? '' : me.req.body.data.id + "'");
-      connection.query(sql, function (err, result, fields) {
-        if (err) {
-          me.res.send({status: 'failure', message:err.message, ppp:sql});
-        } else {
-          if (result && result.length) {
-            me.res.send({status: 'success', data: result});
-          }
-        }
+
+      eng.queryOnly(sql, (resultData)=> {
+        me.res.send(resultData);
       });
-      connection.end();
     }
   }
   module.exports  = usersAdmin;
