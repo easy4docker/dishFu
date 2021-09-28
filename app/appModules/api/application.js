9a4c483da6159e5f7fa5a9a5a3d70e06a0b6f77b
easy4docker <easy4docker@gmail.com>
2021-09-27 18:32:51 -0700

    Update application.js

--- app/appModules/api/application.js
+++ app/appModules/api/application.js
@@ -6,6 +6,13 @@ class Application {
   }
   save() {
     const me = this;
+    me.generateCA((ca)=>{
+      me.saveToDb(ca);
+    });
+  }
+
+  saveToDb(ca) {
+    const me = this;
     const mapping = {
       roles : 'roles' ,
       name : 'name' ,
@@ -14,6 +21,8 @@ class Application {
       description: 'description',
       phone: 'phone',
       qualification: 'qualification',
+      privateKey : ()=>  ca.privateKey,
+      publicKey : ()=>  ca.publicKey,
       created: ()=>  new Date(),
       status: ()=> 0
     }
@@ -28,6 +37,46 @@ class Application {
     eng.queryInsert(sql, [[values]], (result)=> {
       me.res.send(result)
     })
+  }
+
+  generateCA(callback) {
+    const forge = require('node-forge');
+    const fs = require('fs');
+    forge.options.usePureJavaScript = true; 
+
+    var pki = forge.pki;
+    var keys = pki.rsa.generateKeyPair(2048);
+    var cert = pki.createCertificate();
+
+    cert.publicKey = keys.publicKey;
+    cert.serialNumber = '01';
+    cert.validity.notBefore = new Date();
+    cert.validity.notAfter = new Date();
+    cert.validity.notAfter.setFullYear(cert.validity.notBefore.getFullYear()+1);
+
+    var attrs = [
+        {name:'commonName',value:'foodie.com'}
+        ,{name:'countryName',value:'US'}
+        ,{shortName:'ST',value:'CA'}
+        ,{name:'localityName',value:'San Ramon'}
+        ,{name:'organizationName',value:'Test'}
+        ,{shortName:'OU',value:'Test'}
+    ];
+    cert.validity.notBefore = new Date();
+    cert.setSubject(attrs);
+    cert.setIssuer(attrs);
+    cert.sign(keys.privateKey);
+
+    callback({
+      privateKey  : pki.privateKeyToPem(keys.privateKey),
+      publicKey   : pki.publicKeyToPem(keys.publicKey),
+      cert        : pki.certificateToPem(cert)
+    });
+    /*
+    var privateKey = pki.privateKeyToPem(keys.privateKey);
+    var pem_pkey = pki.publicKeyToPem(keys.publicKey);
+    var pem_cert = pki.certificateToPem(cert);
+    */
   }
   actionError() {
     const me = this;
