f14a047d862e6f82087f8e007acea1629c0f1223
easy4docker <easy4docker@gmail.com>
2021-09-28 20:32:35 -0700

    Update cert.js

--- app/appModules/api/cert.js
+++ app/appModules/api/cert.js
@@ -3,10 +3,23 @@ class Cert {
     this.req = req;
     this.res = res;
     this.next = next;
+    this.fs = require('fs');
+    this.forge = require('node-forge');
+  }
+  rootPrivateKey(callback) {
+    const me = this;
+    var pki = me.forge.pki;
+    me.fs.readFile('/var/_rootCert/privateKey.key', 'utf-8', (err, privatePem)=> {
+      const rootPrivateKey = pki.privateKeyFromPem(privatePem);
+      callback(rootPrivateKey);
+    });
   }
   requestCertificate() {
     const me = this;
-    me.res.send(['requestCertificate'])
+    var pki = forge.pki;
+    const cert = pki.certificateFromPem(me.req.body.data.selfcCert)
+    me.res.send(['cert.validity']);
+    // me.res.send(['requestCertificate'])
   }
   actionError() {
     const me = this;
